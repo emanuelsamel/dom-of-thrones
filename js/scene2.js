fdd9fab6b12621d1f5a0e97e98746a046eee17aa
emanuelsamel <mannys94@hotmail.com>
2017-07-10 00:24:45 -0400

    css finished

---
+++ js/scene2.js
@@ -0,0 +1,39 @@
+function scene2() {
+var castle = document.querySelector('#castle');
+
+var stageImg = stage.querySelector('#stage-img');
+
+var castleImgSrc = castle.getAttribute('src');
+
+stageImg.setAttribute('src', castleImgSrc)
+
+
+  // Setup Stage
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
